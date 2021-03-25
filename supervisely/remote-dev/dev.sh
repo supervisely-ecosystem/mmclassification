c2f62cbb4f112c6553403e0122cd4e90b955052e
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-03-25 15:57:47 +0300

    wip

---
+++ supervisely/remote-dev/dev.sh
@@ -0,0 +1,6 @@
+IMAGE=supervisely/base-py
+
+docker pull $IMAGE && \
+docker build --build-arg IMAGE=$IMAGE -t $IMAGE"-debug" . && \
+docker run --rm -it -p 7777:22 --shm-size='1G' -e PYTHONUNBUFFERED='1' $IMAGE"-debug"
+# -v ~/max:/workdir
