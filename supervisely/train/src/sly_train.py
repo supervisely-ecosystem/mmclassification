6d60fa007ad306e85eced6da4a3a49c157327282
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-03-30 15:51:12 +0300

    wip

--- supervisely/train/src/sly_train.py
+++ supervisely/train/src/sly_train.py
@@ -29,17 +29,26 @@ def main():
 # num_classes=1000
 #
 # _base_/datasets
-# size=(256, -1) - input size
+# size=(256, -1) - input size - OK
+# random crop - 224 x 224 ??? - OK
+
+# epochs  - OK
 # resize pillow in VGG???
-# dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
-# samples_per_gpu=32,
-# workers_per_gpu=2
+# dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'), - OK
+# vertical flip - OK
+
+
+# samples_per_gpu=32, - OK
+# workers_per_gpu=2 - OK
 # test=dict( ype=dataset_type, data_prefix='val', pipeline=test_pipeline) - drop test pipeline??
 # sly_dataset - load CLASSES field
 #
 # _base_/schedules
-# max_epochs=100
-# optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0001)
+# max_epochs=100 - OK
+# optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0001) - OK
+
+# grad_clip-????
+
 # lr_config = dict(policy='step', step=[100, 150])
 # lr_config = dict(policy='CosineAnnealing', min_lr=0)
 # lr_config = dict(policy='step', gamma=0.98, step=1)
@@ -50,9 +59,10 @@ def main():
 # load_from = None
 # resume_from = None
 
+#@TODO: total tags, selected tags
+
 
 #@TODO: find configs for models ResNeSt-50 , ResNeSt-101, ResNeSt-200, ResNeSt-269
 #@TODO: for custom weights - load from and resume from???
 if __name__ == "__main__":
-    #sly.main_wrapper("main", main)
-    main() # for debug
+    sly.main_wrapper("main", main)
