f82285901a439456833b344d7f4b438ccb32bbbd
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-03-30 18:21:22 +0300

    optimizer

--- configs/_base_/schedules/supervisely_schedule.py
+++ configs/_base_/schedules/supervisely_schedule.py
@@ -1,15 +1,11 @@
 # optimizer - learn more in pytorch docs
 # https://pytorch.org/docs/1.7.1/optim.html?module-torch.optim
-
 optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 # optimizer = dict(type='Adam', lr=0.001, weight_decay=0.0001)
-
 optimizer_config = dict(grad_clip=None)
 
-# learning policy
-
+# learning policy:
 lr_config = dict(policy='step', step=[30, 60, 90])
 # lr_config = dict(policy='CosineAnnealing', min_lr=0)
 # lr_config = dict(policy='step', gamma=0.98, step=1)
-
 runner = dict(type='EpochBasedRunner', max_epochs=100)
