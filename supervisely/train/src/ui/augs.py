b64a4998878d71a0a1dc6305356bbe3a247f781b
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-05-18 16:29:35 +0300

    load default templates

--- supervisely/train/src/ui/augs.py
+++ supervisely/train/src/ui/augs.py
@@ -1,22 +1,68 @@
+import os
 import supervisely_lib as sly
+import sly_globals as g
+
+_templates = [
+    {
+        "config": "supervisely/train/augs/mmclass-light.json",
+        "name": "Light (color + rotate)",
+    },
+    {
+        "config": "supervisely/train/augs/mmclass-light-with-fliplr.json",
+        "name": "Light + fliplr",
+    },
+    {
+        "config": "supervisely/train/augs/mmclass-heavy-no-fliplr.json",
+        "name": "Heavy",
+    },
+    {
+        "config": "supervisely/train/augs/mmclass-heavy-with-fliplr.json",
+        "name": "Heavy + fliplr",
+    },
+]
+
+
+def _load_template(json_path):
+    config = sly.json.load_json_file(json_path)
+    pipeline = sly.imgaug_utils.build_pipeline(config["pipeline"], random_order=config["random_order"])  # to validate
+    py_code = sly.imgaug_utils.pipeline_to_python(config["pipeline"], config["random_order"])
+    return pipeline, py_code
 
 
 def get_aug_templates_list():
-    return [
-        {
-            "config": "configs/vgg/vgg11_b32x8_imagenet.py",
-            "name": "VGG-11",
-            "color": "light",
-            "blur": True,
-            "noise": True,
-            "cutout": True,
-            "rotate": True,
-            "geometric": 123,
-            "fliplr": False,
-            "flipud": False,
-        },
-    ]
+    pipelines_info = []
+    name_to_py = {}
+    for template in _templates:
+        json_path = os.path.join(g.root_source_dir, template["config"])
+        _, py_code = _load_template(json_path)
+        pipelines_info.append({
+            **template,
+            "py": py_code
+        })
+        name_to_py[template["name"]] = py_code
+    return pipelines_info, name_to_py
+
+
+def get_template_by_name(name):
+    for template in _templates:
+        if template["name"] == name:
+            json_path = os.path.join(g.root_source_dir, template["config"])
+            pipeline, _ = _load_template(json_path)
+            return pipeline
+    raise KeyError(f"Template \"{name}\" not found")
 
 
 def init(data, state):
     state["augsType"] = "template"
+    templates_info, name_to_py = get_aug_templates_list()
+    data["augTemplates"] = templates_info
+    data["augPythonCode"] = name_to_py
+    state["augsTemplateName"] = templates_info[0]["name"]
+
+    data["pyViewOptions"] = {
+        "mode": 'ace/mode/python',
+        "showGutter": False,
+        "readOnly": True,
+        "maxLines": 100,
+        "highlightActiveLine": False
+    }
