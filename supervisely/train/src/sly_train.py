029defca9e91c4b139695fbee033577282429318
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-05-31 20:11:23 +0300

    todos

--- supervisely/train/src/sly_train.py
+++ supervisely/train/src/sly_train.py
@@ -119,6 +119,8 @@ def main():
 #@TODO: --resume-from - continue training
 #@TODO: readme - add py-configs to training artifacts
 #@TODO: readme - tags co-occurrence-matrix
+#Oops! Something went wrong, please try again or contact tech support. Find more info in the app logs. Error: AttributeError("'dict' object has no attribute 'optimizer'")
+# уменьшить скругления + сделать кликабельность по названию?
 
 # implement save_best renaming
 if __name__ == "__main__":
