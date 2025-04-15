af6070dd61b05ddc73b57f424bf1434796298672
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2025-04-15 19:00:01 +0400

    Add experiment support (#40)
    * Add experiment support
    
    * Fix import path for TrainInfo in monitoring.py
    
    * Fix import path for TrainInfo in monitoring.py
    
    * Use asdict to convert experiment_info to dict in create_experiment function
    
    * Add train_size and val_size to experiment info
    
    * Add project preview URL to experiment info JSON in create_experiment function
    
    * Update create_experiment to set output experiment and remove project preview from JSON
    
    * Update Dockerfile to install supervisely version 6.73.346 and add publish script for Docker image
    
    * Update supervisely version to 6.73.347 and adjust related configuration files
    
    * Add error handling for experiment creation in train function
    
    * Add logging for experiment creation in train function

--- supervisely/train/src/ui/monitoring.py
+++ supervisely/train/src/ui/monitoring.py
@@ -1,9 +1,12 @@
 import os
 import supervisely as sly
+from dataclasses import asdict
 from sly_train_progress import init_progress
 import sly_globals as g
 from tools.train import main as mm_train
 from supervisely.io.fs import list_files_recursively
+from supervisely.io.json import dump_json_file
+from supervisely.nn.artifacts.artifacts import TrainInfo
 import workflow as w
 
 _open_lnk_name = "open_app.lnk"
@@ -94,8 +97,7 @@ def upload_artifacts_and_log_progress():
             progress.set_current_value(monitor.bytes_read, report=False)
         _update_progress_ui("UploadDir", g.api, g.task_id, progress)
 
-    progress = sly.Progress("Upload directory with training artifacts to Team Files", 0, is_size=True)
-    progress_cb = partial(upload_monitor, api=g.api, task_id=g.task_id, progress=progress)
+
 
     model_dir = g.sly_mmcls.framework_folder
     remote_artifacts_dir = f"{model_dir}/{g.task_id}_{g.project_info.name}"
@@ -103,9 +105,11 @@ def upload_artifacts_and_log_progress():
 
     local_files = list_files_recursively(g.artifacts_dir)
     remote_files = [file.replace(g.artifacts_dir, remote_artifacts_dir) for file in local_files]
+    total_size = sum([sly.fs.get_file_size(file_path) for file_path in local_files])
 
+    progress = sly.Progress("Upload directory with training artifacts to Team Files", total_size, is_size=True)
+    progress_cb = partial(upload_monitor, api=g.api, task_id=g.task_id, progress=progress)
     g.api.file.upload_bulk(g.team_id, local_files, remote_files, progress_cb=progress_cb)
-    
     g.sly_mmdet_generated_metadata = g.sly_mmcls.generate_metadata(
         app_name=g.sly_mmcls.app_name,
         task_id=g.task_id,
@@ -119,7 +123,24 @@ def upload_artifacts_and_log_progress():
     
     return remote_artifacts_dir
 
-
+def create_experiment(model_name, remote_dir):
+    train_info = TrainInfo(**g.sly_mmdet_generated_metadata)
+    experiment_info = g.sly_mmcls.convert_train_to_experiment_info(train_info)
+    experiment_info.experiment_name = f"{g.task_id}_{g.project_info.name}_{model_name}"
+    experiment_info.model_name= model_name
+    experiment_info.framework_name = f"{g.sly_mmcls.framework_name}"
+    experiment_info.train_size = g.train_size
+    experiment_info.val_size = g.val_size
+    experiment_info_json = asdict(experiment_info)
+    experiment_info_json["project_preview"] = g.project_info.image_preview_url
+    g.api.task.set_output_experiment(g.task_id, experiment_info_json)
+    experiment_info_json.pop("project_preview")
+    
+    experiment_info_path = os.path.join(g.artifacts_dir, "experiment_info.json")
+    remote_experiment_info_path = os.path.join(remote_dir, "experiment_info.json")
+    dump_json_file(experiment_info_json, experiment_info_path)
+    g.api.file.upload(g.team_id, experiment_info_path, remote_experiment_info_path)
+    
 @g.my_app.callback("train")
 @sly.timeit
 @g.my_app.ignore_errors_and_show_dialog_window()
@@ -139,6 +160,13 @@ def train(api: sly.Api, task_id, context, state, app_logger):
         g.api.app.set_fields(g.task_id, fields)
 
         remote_dir = upload_artifacts_and_log_progress()
+        
+        try:
+            sly.logger.info("Creating experiment info")
+            create_experiment(state["selectedModel"], remote_dir)
+        except Exception as e:
+            sly.logger.warning(f"Couldn't create experiment, this training session will not appear in experiments table. Error: {e}")
+            
         file_info = api.file.get_info_by_path(g.team_id, os.path.join(remote_dir, _open_lnk_name))
         api.task.set_output_directory(task_id, file_info.id, remote_dir)
 
